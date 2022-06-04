5c76dabe1019856ac5a5d08ce258c62818f4ff25
wanda <wandaul16@gmail.com>
2022-06-04 21:01:35 +0700

    add FirstScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:project_try/statelesss.dart';
+import  './FirstScreen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+   @override
+  Widget build(BuildContext context) {
+      return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: colors.blue,
+        ),
+        home: FirstScreen();
+      )
+   }
+}
